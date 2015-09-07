fa010e2958b16641de2b843cbbd5fb9a78375c79
Robert Rhoades <robert.rhoades@boxuk.com>
2015-09-07 10:01:19 +0100

    Adds global composer directory to the path

--- fish/config.fish
+++ fish/config.fish
@@ -3,6 +3,7 @@ set -gx PATH /usr/local/bin /usr/local/sbin $PATH
 
 # Composer
 set -gx PATH vendor/bin $PATH
+set -gx PATH $HOME/.composer/vendor/bin $PATH
 
 alias grep "grep --color"
 alias rm "echo 'Don\'t use this use rmtrash instead'"
