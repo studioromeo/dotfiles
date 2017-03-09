3ad5cbe32ed8324c30fcbe96827e05145fd80b6f
Robert Rhoades <robert.rhoades@boxuk.com>
2017-03-09 16:31:22 +0000

    Update the fish config

--- fish/config.fish
+++ fish/config.fish
@@ -4,9 +4,13 @@ set -gx PATH /usr/local/bin /usr/local/sbin $PATH
 # Composer
 set -gx PATH vendor/bin $PATH
 set -gx PATH $HOME/.composer/vendor/bin $PATH
+set -gx PATH node_modules/.bin $PATH
+
+# Brew Cask
+set -gx HOMEBREW_CASK_OPTS --appdir=~/Applications
 
 alias grep "grep --color"
-alias rm "echo 'Don\'t use this use rmtrash instead'"
+alias music "open -n -a 'Google Chrome' --args '--app=https://music.google.com' '--profile-directory=Profile 1'"
 
 set fish_greeting
 
