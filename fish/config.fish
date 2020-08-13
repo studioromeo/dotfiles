d8a6f01ce5db330532e201acc3e4d2f6fdeb679c
Robert Rhoades <robert.rhoades@boxuk.com>
2020-08-13 14:29:57 +0100

    Update shell config

--- fish/config.fish
+++ fish/config.fish
@@ -12,12 +12,14 @@ set fish_greeting
 
 status --is-interactive; and . (rbenv init -|psub)
 
-# Init node
-bass source ~/.nvm/nvm.sh
+set -x NVM_DIR ~/.nvm
+
+nvm use default --silent
 
 # Andorid setup
-set -gx JAVA_HOME (/usr/libexec/java_home) 
-set -gx GRADLE_HOME /usr/local/Cellar/gradle/5.2.1/bin
+set -gx JAVA_HOME /Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home
+
+set -gx GRADLE_HOME /usr/local/Cellar/gradle/5.4.1/bin
 set -gx ANDROID_SDK_ROOT $HOME/Library/Android/sdk
 set -gx PATH $ANDROID_SDK_ROOT/platform-tools $PATH
 set -gx PATH $ANDROID_SDK_ROOT/emulator $PATH
@@ -25,3 +27,7 @@ set -gx PATH $ANDROID_SDK_ROOT/tools/bin $PATH
 
 # Aliases
 alias grep "grep --color"
+set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+
+# Node
+set -gx NODE_ENV development
